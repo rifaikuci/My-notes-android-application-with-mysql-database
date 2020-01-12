904c564d7f4bab54a4cd9da91f1aa47c2f329f2d
Rifai Kuçi <rfai4747@hotmail.com>
2020-01-12 14:13:16 +0300

    Initial commit

---
+++ app/src/main/java/com/rifaikuci/yeni/yerler/mynotesandroidapplicationwithmysqldatabase/MainActivity.java
@@ -0,0 +1,14 @@
+package com.rifaikuci.yeni.yerler.mynotesandroidapplicationwithmysqldatabase;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
