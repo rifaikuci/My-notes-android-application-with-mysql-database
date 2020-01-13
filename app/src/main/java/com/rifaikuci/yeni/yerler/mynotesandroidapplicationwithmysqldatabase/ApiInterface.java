5605a388caf6ace3c3ca5f2d717e81b57b205f79
Rifai Kuçi <rfai4747@hotmail.com>
2020-01-13 16:34:20 +0300

    php dosyaları yazıldı, bağlantılar kuruldu, spectrum.palette kullanıldı.

---
+++ app/src/main/java/com/rifaikuci/yeni/yerler/mynotesandroidapplicationwithmysqldatabase/ApiInterface.java
@@ -0,0 +1,17 @@
+package com.rifaikuci.yeni.yerler.mynotesandroidapplicationwithmysqldatabase;
+
+import retrofit2.Call;
+import retrofit2.http.Field;
+import retrofit2.http.FormUrlEncoded;
+import retrofit2.http.POST;
+
+public interface ApiInterface {
+
+    @FormUrlEncoded
+    @POST("/save.php")
+    Call<Note> saveNote(
+        @Field("title") String title,
+        @Field("note") String note,
+        @Field("color") int color
+    );
+}
--- app/src/main/java/com/rifaikuci/yeni/yerler/mynotesandroidapplicationwithmysqldatabase/MainActivity.java
+++ app/src/main/java/com/rifaikuci/yeni/yerler/mynotesandroidapplicationwithmysqldatabase/MainActivity.java
@@ -2,13 +2,29 @@ package com.rifaikuci.yeni.yerler.mynotesandroidapplicationwithmysqldatabase;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 public class MainActivity extends AppCompatActivity {
+
+    FloatingActionButton fab;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        fab = (FloatingActionButton) findViewById(R.id.add);
+
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(),EditorActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
